941768974bffe32476d05edfbe6035014a8c134e
heislerlemos <heisler.stlamo@gmail.com>
2022-12-14 09:18:53 +0100

    fixed the current user view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,29 +2,32 @@ class HomeController < ApplicationController
 
   before_action :authenticate_user!
   http_basic_authenticate_with name: "clemente", password: "admin2022user", only: :create
-
   
+
   def index 
+    
     @link= Link.new
-    @firstlink = Link.last
+    @firstlink = current_user.links.last
+
+    
+
   end
 
-
-
-
  def create   
-    @link = Link.new(link_params)   
+    
+    @link = Link.new(link_params.merge(user_id: current_user.id))   
     if @link.save   
-      flash[:notice] = 'Product added!'   
       redirect_to root_path   
     else   
-      flash[:error] = 'Failed to edit product!'   
-            redirect_to root_path   
+      redirect_to root_path   
+    
     end   
   end   
-   
+
+
 
 private
+
   def link_params   
     params.require(:link).permit(:url, :user_id)   
   end   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 #  get "links", to: "homes#index"
   get "links", to: "home#index"
   post "links", to: "home#create"
+  put "links/:id", to: 'home#update'
+
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
